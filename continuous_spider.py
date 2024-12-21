a6406513b220f5cfd43b230be19faa1d2e09b8ba
Daniel Na <danielna722@gmail.com>
2024-12-21 12:45:07 -0800

    TIC-3: Filter feature added

--- continuous_spider.py
+++ continuous_spider.py
@@ -6,6 +6,13 @@ from scrapy import signals
 import smtplib
 from threading import Thread
 import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+SPIDERS_DIRECTORY = "C:/Daniel Na/Meme Coin Scraper/main/main/spiders"
 
 def sendMessage(to_phone, msg):
     message = msg + " is a money glitch?!?!?!"
@@ -22,30 +29,75 @@ def sendMessage(to_phone, msg):
     server.close()
 
 def run_spider(wallet_address):
-    spider_directory = "C:/Daniel Na/Meme Coin Scraper/main/main/spiders"
-
-    command = ["scrapy", "crawl", "MemeCoin", "-a", f"start_url={wallet_address}"]
-    print(command)
-
-    subprocess.run(command, cwd=spider_directory)
+    command = ["scrapy", "crawl", "MemeCoin", "-a", f"start_url={wallet_address}", "-L", "ERROR"]
+    process = subprocess.Popen(command, cwd=SPIDERS_DIRECTORY)
+    process.wait()
+    print("Done with run_spider")
 
 #Given wallet address, check to see if the newest transaction matches the corresponding value in the map
 # If different, return False. If same, return True
 # Update the map if different
 def same_address_transaction_map(wallet_address, address_transaction_map):
-    file = open("C:/Daniel Na/Meme Coin Scraper/main/main/spiders/" + wallet_address + ".txt", "r+")
+    file = open(SPIDERS_DIRECTORY + "/addresses/" + wallet_address + ".txt", "r+")
     transactions = file.readlines()
     file.close()
+    
     latest_transaction = transactions[-1]
-    if latest_transaction == address_transaction_map[wallet_address]:
-        return True
+    print("LATEST TRANSACTION: ",  latest_transaction, " \nThe Transaction in the map: ", address_transaction_map[wallet_address])
+    time.sleep(5)
+    if address_transaction_map[wallet_address] is not None:
+        if latest_transaction[0:10] == address_transaction_map[wallet_address][0:10]:
+            print("The map is up to date (the transaction in the file is equal to map)")
+            return True
+        else:
+            print("The map is NOT up to date (the transaction in the file is NOT equal to map)")
+            address_transaction_map[wallet_address] = latest_transaction
+            return False
     else:
         # map is not in sync with the file
+        print("The map is NOT up to date (the transaction in the file is NOT equal to map)")
         address_transaction_map[wallet_address] = latest_transaction
         return False
 
+def is_transaction_buy(transaction_signature, wallet_address):
+    url = "https://explorer.solana.com/tx/" + transaction_signature
+    driver = webdriver.Chrome()
+    driver.get(url)
+    text = ""
 
+    try:
+        # Wait for the element to load and have text
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="ix-3"]/div[2]/table/tbody/tr[3]/td[2]/div[2]/span[2]/a'))
+        )
+        # Fetch and print the text
+        text = element.text
+        if not text:
+            text = driver.execute_script("return arguments[0].textContent;", element)
+        print(f"From Address: {text}")
+    except Exception as e:
+        print(f"Error: {e}")
+    driver.quit()
 
+    return text == wallet_address
+
+    # command = ["scrapy", "crawl", "TransactionSpider", "-a", f"signature={transaction_signature}", "-a",f"buyer_address={wallet_address}", "-L", "ERROR"]
+    # print("Starting crawl for TransactionSpider")
+    # time.sleep(5)
+    # process = subprocess.Popen(command, cwd=SPIDERS_DIRECTORY)
+
+    # process.wait()
+
+    # print("CHECKING TRANSACTION SIGNATURE...")
+    # try:
+    #     file1 = open(SPIDERS_DIRECTORY+ "/isTransactionBuy.txt", "r+")
+    #     answer = file1.readline()
+    #     if answer == "True":
+    #         return True
+    #     else:
+    #         return False
+    # except:
+    #     return False
 
 
 def run_spider_continuously(wallet_addresses, recipients):
@@ -64,7 +116,7 @@ def run_spider_continuously(wallet_addresses, recipients):
 
     for wallet_address in wallet_addresses:
         try:
-            file = open("C:/Daniel Na/Meme Coin Scraper/main/main/spiders/" + wallet_address + ".txt", "r+")
+            file = open(SPIDERS_DIRECTORY+ "/addresses/" + wallet_address + ".txt", "r+")
             transactions = file.readlines()
             if len(transactions) > 0:
                 address_transaction_map[wallet_address] = transactions[-1]
@@ -72,7 +124,7 @@ def run_spider_continuously(wallet_addresses, recipients):
                 address_transaction_map[wallet_address] = None
             file.close()
         except:
-            file = open("C:/Daniel Na/Meme Coin Scraper/main/main/spiders/" + wallet_address + ".txt", "w+")
+            file = open(SPIDERS_DIRECTORY+ "/addresses/" + wallet_address + ".txt", "w+")
             address_transaction_map[wallet_address] = None
             file.close()
         
@@ -80,17 +132,14 @@ def run_spider_continuously(wallet_addresses, recipients):
     while True:
         try:
             for wallet_address in wallet_addresses:
-                thread = Thread(target = run_spider, args=(wallet_address,))
-                thread.start()
-                thread.join()
-                time.sleep(5)  # Adjust delay between spider runs if needed
+                run_spider(wallet_address)
                 if not same_address_transaction_map(wallet_address, address_transaction_map):
-                    for r in recipients:
-                        sendMessage(name_number_map[r], wallet_address)
-
-                
-                
-            
+                    transaction_signature = address_transaction_map[wallet_address]
+                    if is_transaction_buy(transaction_signature, wallet_address):
+                        print("About to send messages: ")
+                        for r in recipients:
+                            sendMessage(name_number_map[r], wallet_address)
+                time.sleep(5)        
         except KeyboardInterrupt:
             break
 
@@ -98,3 +147,12 @@ if __name__ == "__main__":
     wallet_addresses = input("Enter wallet address(es)-separate with space if needed: ").split(" ")
     recipients = input("Enter recipients (Daniel, Matua, Owen, Krish): ").split()
     run_spider_continuously(wallet_addresses, recipients)
+
+
+
+
+
+
+#'//div[@id="ix-3"]/div[@class="table-responsive mb-0"]/table[@class="table table-sm table-nowrap card-table"]/tbody[@class="list]/tr[3]/td[@class="text-lg-end"]/div[@class="d-flex d-lg-none align-items-center"]/span[@class="font-monospace"]/a'
+#F1f2duDG5DnHEuFguhXqdijh5T39ttmkvnNFReDNSWRM
+#//*[@id="ix-3"]/div[2]/table/tbody/tr[3]/td[2]/div[2]/span[2]/a
--- main/main/spiders/meme_coin_spider.py
+++ main/main/spiders/meme_coin_spider.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from scrapy.selector import Selector
 import time
 
+DIRECTORY = "C:/Daniel Na/Meme Coin Scraper/main/main/spiders/addresses/"
+
 class MemeCoinSpider(Spider):
     name = 'MemeCoin'
 
@@ -13,6 +15,8 @@ class MemeCoinSpider(Spider):
 
         self.wallet_address = [kwargs.get('start_url')]
         self.url = "https://explorer.solana.com/address/" + self.wallet_address[0]
+        self.file_name = DIRECTORY + self.wallet_address[0]
+
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=chrome_options)
@@ -26,21 +30,26 @@ class MemeCoinSpider(Spider):
 
     def parse(self, response):
         self.driver.get(response.url)
-        time.sleep(5)  # Allow JavaScript to load
+        time.sleep(10)  # Allow JavaScript to load
         sel = Selector(text=self.driver.page_source)
-        temp = sel.xpath('//div[@class="d-flex align-items-center "]/span[@class="font-monospace"]/a/text()').get()
+        temp = sel.xpath('//div[@class="d-flex align-items-center "]/span[@class="font-monospace"]/a/@href').get()
+        temp = temp[4:]
+        if temp is None:
+            print("Couldnt get the data in time :( Im a naughty spider")
+            return
         self.driver.quit()
+        print(temp)
         
-        file1 = open(self.wallet_address[0] + ".txt", 'r+')
+        file1 = open(self.file_name + ".txt", 'r+')
         transactions = file1.readlines()
         if len(transactions) == 0:
             file1.close()
-            file1 = open(self.wallet_address[0] + ".txt", 'a')
-            file1.write("\n" + temp)
-        elif temp != transactions[-1]:
+            file1 = open(self.file_name + ".txt", 'a', encoding="utf-8")
+            file1.write(temp + "\n")
+        elif temp != transactions[-1].strip():
             file1.close()
-            file1 = open(self.wallet_address[0] + ".txt", 'a')
-            file1.write("\n" + temp)
+            file1 = open(self.file_name + ".txt", 'a', encoding="utf-8")
+            file1.write(temp + "\n")
         file1.close()
 
         yield {
